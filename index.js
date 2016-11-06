6941d1b8be3a3ff32d4d63105c952a85f032c6cc
Peter Beshai <peter.beshai@gmail.com>
2016-11-05 21:19:17 -0700

    Remove old index

--- index.js
+++
@@ -1,9 +0,0 @@
-export { default as extentLimited } from './src/extentLimited';
-export { default as extentMulti } from './src/extentMulti';
-export { default as findClosestSorted } from './src/findClosestSorted';
-export { default as findClosestUnsorted } from './src/findClosestUnsorted';
-export { default as findEqualSorted } from './src/findEqualSorted';
-export { default as findEqualUnsorted } from './src/findEqualUnsorted';
-export { default as rectContains } from './src/rectContains';
-export { default as rectIntersects } from './src/rectIntersects';
-export { default as rotate } from './src/rotate';
