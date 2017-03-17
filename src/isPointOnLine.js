d98b705808dceabefb5e0912cd6cc9d9fe0b184e
Peter Beshai <peter.beshai@gmail.com>
2017-03-17 16:35:59 -0400

    import distance into file

--- src/isPointOnLine.js
+++ src/isPointOnLine.js
@@ -1,3 +1,5 @@
+import distance from './distance';
+
 export default function isPointOnLine(line, point) {
   const epsilon = 1e-10;
   return Math.abs((distance(line[0], point) + distance(line[1], point)) -
